9605cd019fbc84ee3781d50e0e50b4f28014c4fe
Mariya Leshcheva <maleshch@icloud.com>
2023-05-21 19:22:12 +0300

    Изменён сценарий тестов в классе ConstructorTest. Обновлён отчёт.

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,17 +1,15 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class MainPage {
 
+    private static final String selectedButtonClassName = "current";
     private final By buttonEnterAccount = By.xpath(".//button[text()='Войти в аккаунт']");
-    private final By buttonBun = By.xpath(".//span[text()='Булки']");
-    private final By buttonSouse = By.xpath(".//span[text()='Соусы']");
-    private final By buttonIngredient = By.xpath(".//span[text()='Начинки']");
-    private final By menuContainerIngredients = By.xpath(".//div[starts-with(@class, 'BurgerIngredients_ingredients__menuContainer__')]");
+    private final By buttonBun = By.xpath(".//span[text()='Булки']/..");
+    private final By buttonSauce = By.xpath(".//span[text()='Соусы']/..");
+    private final By buttonIngredient = By.xpath(".//span[text()='Начинки']/..");
     private final By title = By.xpath(".//h1[text() = 'Соберите бургер']");
     private final WebDriver driver;
     public MainPage(WebDriver driver) {
@@ -24,29 +22,26 @@ public class MainPage {
     public void clickButtonBun() {
         driver.findElement(buttonBun).click();
     }
-    public void clickButtonSouse() {
-        driver.findElement(buttonSouse).click();
+    public void clickButtonSauce() {
+        driver.findElement(buttonSauce).click();
     }
     public void clickButtonIngredient() {
         driver.findElement(buttonIngredient).click();
-    }
-
-    public double getMenuContainerScrollTop() {
-        return getScrollTop(driver.findElement((menuContainerIngredients)));
     }
 
     public boolean isTitleDisplayed() {
         return driver.findElement(title).isDisplayed();
     }
 
-    private double getScrollTop(WebElement element) {
-        JavascriptExecutor javascriptExecutor = ((JavascriptExecutor)driver);
+    public boolean isBunSelected() {
+        return driver.findElement(buttonBun).getAttribute("class").contains(selectedButtonClassName);
+    }
 
-        try {
-            return  (double)javascriptExecutor.executeScript("return arguments[0].scrollTop;", element);
-        } catch (ClassCastException exception) {
-        }
+    public boolean isSauceSelected() {
+        return driver.findElement(buttonSauce).getAttribute("class").contains(selectedButtonClassName);
+    }
 
-        return 0;
+    public boolean isIngredientSelected() {
+        return driver.findElement(buttonIngredient).getAttribute("class").contains(selectedButtonClassName);
     }
 }
--- src/test/java/ConstructorTest.java
+++ src/test/java/ConstructorTest.java
@@ -3,14 +3,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import io.qameta.allure.junit4.DisplayName;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import pages.MainPage;
 
-import java.time.Duration;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ConstructorTest extends BaseTest {
 
@@ -25,54 +20,31 @@ public class ConstructorTest extends BaseTest {
 
     @Test
     @DisplayName("Переход к соусам")
-    public void clickOnSousesSectionButtonAutoScroll() {
-        double oldScrollTop = mainPage.getMenuContainerScrollTop();
-        mainPage.clickButtonSouse();
-        waitForScrollChange(oldScrollTop);
-        double newScrollTop = mainPage.getMenuContainerScrollTop();
-
-        assertNotEquals("Автоскролл к соусам не произошёл", oldScrollTop, newScrollTop);
+    public void clickOnSaucesSectionButton() {
+        mainPage.clickButtonSauce();
+        boolean actual = mainPage.isSauceSelected();
+        assertTrue("Переход к начинке не произошёл", actual);
     }
 
     @Test
     @DisplayName("Переход к начинке")
-    public void clickOnIngredientsSectionButtonAutoScroll() {
-        double oldScrollTop = mainPage.getMenuContainerScrollTop();
-        mainPage.clickButtonSouse();
-        waitForScrollChange(oldScrollTop);
-        double newScrollTop = mainPage.getMenuContainerScrollTop();
-
-        assertNotEquals("Автоскролл к ингредиентам не произошёл", oldScrollTop, newScrollTop);
+    public void clickOnIngredientsSectionButton() {
+        mainPage.clickButtonIngredient();
+        boolean actual = mainPage.isIngredientSelected();
+        assertTrue("Переход к начинке не произошёл", actual);
     }
 
     @Test
     @DisplayName("Возвращение к булкам")
-    public void clickOnBunsSectionButtonAutoScroll() {
-        double initialScrollTop = mainPage.getMenuContainerScrollTop();
+    public void clickOnBunsSectionButton() {
         mainPage.clickButtonIngredient();
-        waitForScrollChange(initialScrollTop);
-        double oldScrollTop = mainPage.getMenuContainerScrollTop();
         mainPage.clickButtonBun();
-        waitForScrollChange(oldScrollTop);
-        double newScrollTop = mainPage.getMenuContainerScrollTop();
-
-        assertNotEquals("Автоскролл к булкам не произошёл", oldScrollTop, newScrollTop);
+        boolean actual = mainPage.isBunSelected();
+        assertTrue("Переход к булкам не произошёл", actual);
     }
 
     @After
     public void tearDown() {
         super.tearDown();
-    }
-
-    private void waitForScrollChange(double scrollTop) {
-        Wait<WebDriver> wait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(5))
-                .pollingEvery(Duration.ofSeconds(1));
-
-        try {
-            wait.until(driver -> mainPage.getMenuContainerScrollTop() != scrollTop);
-        } catch (TimeoutException exception) {
-
-        }
     }
 }
